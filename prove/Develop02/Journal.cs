c5af9ef596159da382d19c81dc86e9090a02e460
samuelkwota <samuelkwota.sk@gmail.com>
2023-09-20 11:01:28 -0800

    update

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    public List<Entry> _entries;
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void displayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
+
+    
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,13 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class PromptGenerator
+{
+    public List<string> _prompt;
+
+    public string GetRandonPromt()
+    {
+         return ""; 
+    }
+     
+    
+}
